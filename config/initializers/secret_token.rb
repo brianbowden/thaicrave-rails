c5e9ac1b1a544d42c68bbda59bc054ab4b71eb16
Brian Bowden <brian@bitstomp.com>
2013-07-24 01:12:44 -0400

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThaicraveRails::Application.config.secret_key_base = '1394b3c860401fbb7b56b06ff25190bb643dc825dd154da7fee03e544ae92c44e6377cffc5f826a575e299bdde57e9dfc622bcf0b1cc12a5e5b888b4703c489e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThaicraveRails::Application.config.session_store :cookie_store, key: '_thaicrave-rails_session'
