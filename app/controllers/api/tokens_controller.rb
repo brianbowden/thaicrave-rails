4df4d529408a2f657da192881f72bf0f0a70049f
Brian Bowden <brian@bitstomp.com>
2013-12-31 14:48:36 -0500

    Updated Devise usage to cope with removal of Authentication Token support in Devise.

--- app/controllers/api/tokens_controller.rb
+++ app/controllers/api/tokens_controller.rb
@@ -1,5 +1,5 @@
 class Api::TokensController < ApplicationController
-    skip_before_filter :verify_authenticity_token
+    skip_before_filter :authenticate_user_from_token!
     respond_to :json
 
     def create
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
 
+  before_filter :authenticate_user_from_token!
+  before_filter :authenticate_user!
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :first_name, :last_name
   end
@@ -14,9 +18,19 @@ class ApplicationController < ActionController::Base
   prepend_before_filter :get_auth_token
 
   private
+  
   def get_auth_token
     if auth_token = params[:auth_token].blank? && request.headers["X-AUTH-TOKEN"]
       params[:auth_token] = auth_token
+    end
+  end
+
+  def authenticate_user_from_token!
+    user_email = params[:user_email].presence
+    user       = user_email && User.find_by_email(user_email)
+
+    if user && Devise.secure_compare(user.authentication_token, get_auth_token)
+      sign_in user, store: false
     end
   end
 end
